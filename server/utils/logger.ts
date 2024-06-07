43587a6747fb01a94b56ea2ba29b319a11f21573
jeetpateljsp <jeet.patel@tntra.io>
2024-06-07 19:18:05 +0530

    refactor (logger): simplified log file

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -4,30 +4,18 @@ import * as fs from 'node:fs';
 
 const logger = (name: string) => {
     const date = new Date();
-    const logFileName = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}.log`;
-    const logFilePath = path.join('logs', logFileName);
+    const logFilePath = path.join('logs', `${new Date().toDateString()}.log`);
 
-    const logDir = path.dirname(logFilePath);
-    if (!fs.existsSync(logDir)) {
-        fs.mkdirSync(logDir, { recursive: true });
+    if (!fs.existsSync('logs')) {
+        fs.mkdirSync('logs', { recursive: true });
     }
 
     return bunyan.createLogger({
         name,
         streams: [
-            {
-                level: 'debug',
-                path: logFilePath,
-                type: 'rotating-file'
-            },
-            {
-                level: 'info',
-                stream: process.stdout
-            },
-            {
-                level: 'error',
-                path: 'error.log'
-            }
+            { level: 'debug', path: logFilePath, },
+            { level: 'info', stream: process.stdout },
+            { level: 'error', path: 'error.log' }
         ],
         src: true, // Include the source code in the log. This is useful for debugging
         hostname: 'localhost', // Replace with os.hostname() if you want to log the hostname
